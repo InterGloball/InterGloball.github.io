cb18e520263c1e312a1ed9704496be3e4be77d1a
pozchel <62568601+pozchel@users.noreply.github.com>
2020-04-12 00:13:40 +0600

    Commit intergloball

---
+++ test/js.js
@@ -0,0 +1,43 @@
+		let button = document.querySelector('#submit');
+		button.addEventListener('click', function() {
+			let forms = document.querySelectorAll('#test form');
+
+			for (let form of forms) {
+				answs = ['A', 'B', 'C']	
+				form.classList.remove('correct');
+				form.classList.remove('incorrect');
+
+				let inputs = form.querySelectorAll('input');
+
+
+				for (let input of inputs) {
+
+					if (input.checked){
+						if (input.name == 1 ) {
+							if (input.value == answs[0]) {
+								form.classList.add('correct')
+							}else {
+								form.classList.add('incorrect')
+							}		
+						};	
+						if (input.name == 2 ) {
+							if (input.value == answs[1]) {
+								form.classList.add('correct')
+							} else {
+								form.classList.add('incorrect')
+							}		
+						};	
+						};
+						input.classList.remove('correct');
+						input.classList.remove('incorrect');
+
+						if (input.value.toLowerCase() == input.dataset.right){
+							input.classList.add('correct');
+						} else {
+							input.classList.add('incorrect');
+						}
+					}
+
+
+				}
+			});
